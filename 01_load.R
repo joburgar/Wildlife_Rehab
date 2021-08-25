7ae1bae8c88002f169d3de84b03b8beacb71985b
Burgar <Joanna.Burgar@gov.bc.ca>
2021-08-24 17:56:47 -0700

    Creating list on non MBA bird families.

--- 01_load.R
+++ 01_load.R
@@ -26,5 +26,66 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 
+###---
+# Provincially mandated wildlife (birds)
+# Birds of the following species:
+#   
+# (a)all species of birds described in the American Ornithologists Union Checklist of North America Birds, 7th edition (American Ornithologists Union 1999) or its supplements, which are native to Canada or the United States of America and were not introduced by man;
+# 
+# (b)Alauda arvensis — skylark;
+# 
+# (c)Acridotheres cristatellus — crested myna;
+# 
+# (d)Passer domesticus — house sparrow;
+# 
+# (e)Sturnus vulgaris — European starling;
+# 
+# (f)Columba livia — rock dove (except domestic-bred racing pigeon).
 
-.libPaths()
+# download directly as csv (American Ornithologist checklist)
+# http://checklist.americanornithology.org/taxa/
+# A accidental/casual in AOU area
+# H recorded in AOU area only from Hawaii
+# I introduced into AOU area
+# N has not bred in AOU area, but occurs regularly as nonbreeding visitor
+# † extinct
+# * probably misplaced in the current phylogenetic listing, but data indicating proper placement are not yet available
+
+NACC <- read.csv("http://checklist.americanornithology.org/taxa.csv?type=charset%3Dutf-8%3Bsubspecies%3Dno%3B/NACC_list_species.csv",
+                 na.strings=c("NA","NaN", ""))
+glimpse(NACC)
+head(NACC)
+NACC <- NACC %>% filter(is.na(status_accidental)) %>% filter(is.na(status_extinct))%>%
+  filter(is.na(status_hawaiian)) %>%  filter(is.na(status_introduced))%>%
+  filter(is.na(status_misplaced))%>%  filter(is.na(status_nonbreeding))
+
+NACC %>% count(family)
+
+# Families not covered under the migratory bird act
+# Accipitridae (osprey, kite, eagles, harrier, hawks)
+# Alcedinidae (kingfisher)
+# Anhingidae (anhinga)
+# Aramidae (limpkin)
+# Cathartidae (vultures)
+# Corvidae (jays, crows and ravens)
+# Falconidae (caracara, falcons)
+# Fregatidae (frigatebirds)
+# Odontophoridae (quail)
+# Passeridae (weaver finches)
+# Pelecanidae (pelicans)
+# Phaethontidae (tropicbirds)
+# Phalacrocoracidae (cormorants)
+# Phasianidae (partridges, pheasants, grouse, ptarmigan, sage-grouse, prairie-chicken, turkey)
+# Phoenicopteridae (flamingo)
+# Prunellidae (accentor)
+# Ptilogonatidae (silky-flycatcher)
+# Strigidae (owls)
+# Sturnidae (starlings, mynas)
+# Tytonidae (barn owl)
+
+nonMBA <- c("Accipitridae", "Alcedinidae", "Anhingidae", "Aramidae", "Cathartidae", "Corvidae", "Falconidae",
+            "Fregatidae", "Odontophoridae", "Passeridae", "Pelecanidae", "Phaethontidae", "Phalacrocoracidae",
+            "Phasianidae", "Phoenicopteridae", "Prunellidae", "Ptilogonatidae", "Strigidae", "Sturnidae",
+            "Tytonidae")
+
+NACC %>% filter(!family %in% nonMBA) %>% count(family)
